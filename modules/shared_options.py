8943e2659dae8ef75fdf8c7e256d260c021a17a5
w-e-w <40751091+w-e-w@users.noreply.github.com>
2024-07-30 17:48:17 +0900

    option: reduce connecting to huggingface
    for assets if local cache is available
    note: enabling this with prevent the assets from being updated

--- modules/shared_options.py
+++ modules/shared_options.py
@@ -129,6 +129,7 @@ options_templates.update(options_section(('system', "System", "system"), {
     "hide_ldm_prints": OptionInfo(True, "Prevent Stability-AI's ldm/sgm modules from printing noise to console."),
     "dump_stacks_on_signal": OptionInfo(False, "Print stack traces before exiting the program with ctrl+c."),
     "concurrent_git_fetch_limit": OptionInfo(16, "Number of simultaneous extension update checks ", gr.Slider, {"step": 1, "minimum": 1, "maximum": 100}).info("reduce extension update check time"),
+    "hd_dl_local_first": OptionInfo(False, "Prevent connecting to huggingface for assets if cache is available").info('this will also prevent assets from being updated'),
 }))
 
 options_templates.update(options_section(('profiler', "Profiler", "system"), {
